2890f2df17af3cc4cf3cf2a73c0d330b1d474bb8
shingom <shingo.pub@gmail.com>
2020-12-30 11:54:41 +0900

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import * as Lab from '@material-ui/lab';
+
+interface Window {
+  muilab: any
+}
+declare var window: Window
+
+window.muilab = Lab;
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  target: 'web',
+  devtool: 'inline-source-map',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'muilabpub.js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /(node_modules | .git | dist)/,
+      }
+    ]
+  },
+   optimization: {
+        minimize: true,
+    },
+    externals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        '@material-ui/core': 'MaterialUI',
+    }
+}
+
